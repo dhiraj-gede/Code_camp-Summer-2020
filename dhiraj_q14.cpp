8d627bff97192146112e0ad05d306f205a5172cb
dhiraj-gede <42700091+dhiraj-gede@users.noreply.github.com>
2020-05-13 01:57:12 +0530

    Day 7 Solution

--- dhiraj_q14.cpp
+++ dhiraj_q14.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
  int a;
  char b;
-  	cin>>a >> b;
+  	cin>>a >> b;				//for any input of b other than uppercase letter, default output is set to lowercase
   	if(a%2==0)
     		cout<<"even"<<endl;
    	else
@@ -11,6 +11,6 @@ int main(){
    	if(b>=65 && b<=91)
 		cout<<"uppercase";
 	else
-		cout<< "lowercase";
+		cout<< "lowercase";							
   return 0;
 }

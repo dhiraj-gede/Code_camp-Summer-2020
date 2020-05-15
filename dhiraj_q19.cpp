0e87936d2cc6e1d6fb464b6ed4aabdc4bea481e8
dhiraj-gede <42700091+dhiraj-gede@users.noreply.github.com>
2020-05-15 21:25:41 +0530

    Day 10 Solution

---
+++ dhiraj_q19.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 5; j > 0; j--) {
+            if (i >= j) {
+                cout << "* ";
+                
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
